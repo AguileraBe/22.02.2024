bd25a245f6bc33e4368c60a21389f7dbd91d3206
Be <159550318+AguileraBe@users.noreply.github.com>
2024-02-22 12:05:36 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+const calcular = () => {
+    let numero1 = document.getElementById("numero1");
+    let numero1 = document.getElementById("numero1");
+    let numero1 = document.getElementById("numero1");
+
+    resultado.value = parseInt(numero1.value) + parseInt(numero2.value);
+}
